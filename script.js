f0d01c15726bfeec4542da2dc3ec6977850d36f6
Conrad Hunter <conrad.hunter10@gmail.com>
2022-12-06 11:32:05 -0800

    remove un-needed console.logs git push

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const calculateButtonOne = document.getElementById('btn1');
 const calculateButtonTwo = document.getElementById('btn2');
 const calculateButtonThree = document.getElementById('btn3');
 
-
-
 // Box One => what is ''% of #
 
 const boxOneLeft = document.getElementById('boxOne-left');
@@ -13,11 +11,7 @@ const boxOneOutput = document.getElementById('boxOne-output');
 function calculateBoxOne() {
     const boxOneCalculation = (boxOneLeft.value * boxOneRight.value) / 100; 
     boxOneOutput.value = boxOneCalculation;
-    console.log(boxOneCalculation);
-    console.log('calculated box one');
 }
-
-
 
 // Box Two => # is what percentage of #
 
@@ -28,8 +22,6 @@ const boxTwoOutput = document.getElementById('boxTwo-output');
 function calculateBoxTwo() {
     const boxTwoCalculation = (boxTwoLeft.value * 100) / boxTwoRight.value + '%';
     boxTwoOutput.value = boxTwoCalculation;
-    console.log(boxTwoCalculation)
-    console.log('calculated box two')
 }
 
 // Box Three => Percentage increase/decrease from # to #
@@ -43,21 +35,16 @@ function calculateBoxThree() {
     if (boxThreeLeft.value < boxThreeRight.value) {
         const boxThreeIncreaseCalc = (boxThreeRight.value - boxThreeLeft.value) / boxThreeLeft.value * 100 + '%';
         boxThreeOutput.value = boxThreeIncreaseCalc;
-        console.log(boxThreeIncreaseCalc);
     } 
 
     else if (boxThreeLeft.value > boxThreeRight.value) {
         const boxThreeDecreaseCalc = '-' + (boxThreeLeft.value - boxThreeRight.value) / boxThreeLeft.value * 100 + '%';
         boxThreeOutput.value = boxThreeDecreaseCalc;
-        console.log(boxThreeDecreaseCalc);
     }
 
 }
 
 
-
-
 function clearCalculator() {
     location.reload();
-    console.log('cleared the calculator')
 }
